2e326a4b9ce92426496674cb6d95d05015335954
Iinrosmala05 <rosmala01011991@gmail.com>
2026-03-19 09:10:49 +0700

    feat: implement mobile banking UI with balance visibility and menu actions

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,13 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatefulWidget {
-    @override
+  @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   double saldo = 10000000;
+  bool isSaldoVisible = true;
 
   void updateSaldo(double nilai) {
     setState(() {
@@ -30,8 +31,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   String formatRupiah(double angka) {
-    return "Rp ${angka.toStringAsFixed(0).replaceAllMapped(RegExp (r'(\d{1,3})(?=(\d{3})+(?!\d))'),
-    (Match m) => '${m[1]}.')}";
+    return "Rp ${angka.toStringAsFixed(0).replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]}.')}";
   }
 
   @override
@@ -41,123 +41,146 @@ class _HomePageState extends State<HomePage> {
         title: Text("Mobile Banking"),
         centerTitle: true,
         backgroundColor: Colors.blue,
-        actions: [
-          Icon(Icons.notifications),
-          SizedBox(width: 10,)
+        actions: [Icon(Icons.notifications),
+        SizedBox(width: 10),
+        ],
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Beranda"),
+          BottomNavigationBarItem(icon: Icon(Icons.history), label: "Riwayat"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profil"),
         ],
       ),
       body: Column(
         children: [
-          // Container Saldo
+          //Card Saldo
           Container(
-            padding: EdgeInsets.all(20),
+            padding: EdgeInsets.all(15),
             margin: EdgeInsets.all(15),
             decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 5)],
             ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   "Halo, Iin Rosmala",
-                  style:TextStyle(color: Colors.white, fontSize: 16),
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
                 SizedBox(height: 5),
-                Text(
-                  "Saldo Anda",
-                  style: TextStyle(color: Colors.white),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Saldo Anda"),
+                    IconButton(
+                      icon: Icon(
+                        isSaldoVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          isSaldoVisible = !isSaldoVisible;
+                        });
+                      },
+                    ),
+                  ],
                 ),
-                SizedBox(height: 10),
                 Text(
-                  formatRupiah(saldo),
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
+                  isSaldoVisible ? formatRupiah(saldo) : "Rp ******",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                 ),
               ],
             ),
           ),
-        
+
           // Grid Menu
           Expanded(
             child: GridView.count(
-            crossAxisCount: 3,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            padding: EdgeInsets.all(10),
-            children: [
-               menuItem(Icons.send, "Transfer"),
-               menuItem(Icons.phone_android, "pulsa"),
-               menuItem(Icons.receipt, "Tagihan"),
-               menuItem(Icons.account_balance, "Top Up"),
-               menuItem(Icons.history, "Riwayat"),
-               menuItem(Icons.settings, "Pengaturan"),
+              crossAxisCount: 3,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              padding: EdgeInsets.all(10),
+              children: [
+                menuItem(Icons.send, "Transfer"),
+                menuItem(Icons.phone_android, "Pulsa"),
+                menuItem(Icons.receipt, "Tagihan"),
+                menuItem(Icons.account_balance, "Top Up"),
+                menuItem(Icons.history, "Riwayat"),
+                menuItem(Icons.settings, "Pengaturan"),
               ],
             ),
-           ),
-          ], 
-        ),
-      );
-     }
-     
-     Widget menuItem(IconData icon, String title){
-      return InkWell(
-        onTap: () {
-          //contoh interaksi: Update saldo jika Transfer/Top Up di klik
-          if (title == "Transfer") updateSaldo(-500000);
-          if (title == "Top Up") updateSaldo(500000);
-          showDialog(
-            context: context,
-            builder: (context){
-              return Dialog(
-                shape: RoundedRectangleBorder( 
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Container(
-                  padding: EdgeInsets.all(20),          
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(icon, size: 50, color: Colors.blue),
-                      SizedBox(height: 15),
-                      Text(
-                        title,
-                        style:TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      Text("Menu berhasil dipilih"),
-                      SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed:() {
-                          Navigator.pop(context);
-                        },
-                        child: Text("OK"),
-                      )
-                    ],
-                  ),
-                ),
-              );
-            }
-          );
-        },
-        child: Card(
-          margin: EdgeInsets.all(10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(icon, size: 30, color: Colors.blue),
-              SizedBox(height: 10),
-              Text(title),
-            ],
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget menuItem(IconData icon, String title) {
+    return InkWell(
+      onTap: () {
+        //contoh interaksi: Update saldo jika Transfer/Top Up di klik
+        if (title == "Transfer") updateSaldo(-500000);
+        if (title == "Top Up") updateSaldo(500000);
+
+        showDialog(
+          context: context,
+          builder: (context) {
+            return Dialog(
+              insetPadding: EdgeInsets.symmetric(horizontal: 120),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Container(
+                width: 200,
+                padding: EdgeInsets.all(15),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(icon, size: 40, color: Colors.blue),
+                    SizedBox(height: 10),
+                    Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text("Berhasil", style: TextStyle(fontSize: 12)),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text("OK"),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+      child: Card(
+        elevation: 2,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 25, color: Colors.blue),
+            SizedBox(height: 5),
+            Text(title, style: TextStyle(fontSize: 12)),
+          ],
         ),
-      );
-     }
-    }
+      ),
+    );
+  }
+}
